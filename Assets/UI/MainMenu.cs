0b634c5cc55698c56395fcbc5b784a7f2254865b
BuzJr <pawlasnr@gmail.com>
2023-10-13 19:53:47 -0400

    Initial Commit

---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject StartButton;
+    [SerializeField]
+    private GameObject BackButton;
+    [SerializeField]
+    private GameObject[] LevelButtons;
+    [SerializeField]
+    private GameObject QuitButton;
+
+    private void Start()
+    {
+        Back();
+    }
+
+    public void StartGame()
+    {
+        StartButton.SetActive(false);
+        BackButton.SetActive(true);
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].SetActive(true);
+        }
+    }
+
+    public void LevelSelect() // TODO: maybe in the future make this take an int level or maybe enum level as an arg to specifically load a certain level?
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        SceneSwitcher.Quit();
+    }
+
+    // Hide level select and back, show Start and Quit
+    public void Back()
+    {
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].SetActive(false);
+        }
+        BackButton.SetActive(false);
+        StartButton.SetActive(true);
+        QuitButton.SetActive(true);
+    }
+}
