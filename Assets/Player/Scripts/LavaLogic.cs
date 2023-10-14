52816fa34154012f9ac9da0134f5fe0dbe9b36a5
Luke Webb <lukiewebb@hotmail.com>
2023-10-14 15:30:23 -0400

    s

---
+++ Assets/Player/Scripts/LavaLogic.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaLogic : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+      transform.position = new Vector3(0, 0, 0);  
+    }
+    int wait = 0;
+    bool rising = false;
+    // Update is called once per frame
+    void Update()
+    {
+        wait += 1;
+        if (wait == 450) {
+            rising = true;
+            
+        }
+        if (rising == true) {
+            transform.position += new Vector3(0, .01f, 0);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("ass");
+        rising = false;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("titties");
+        rising = true;
+    }
+}
