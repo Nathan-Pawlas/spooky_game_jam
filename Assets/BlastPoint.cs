2ae0b5913d4255973a47ebc4e176250920679ae1
Luke Webb <lukiewebb@hotmail.com>
2023-10-15 16:21:48 -0400

    s

--- Assets/BlastPoint.cs
+++ Assets/BlastPoint.cs
@@ -8,7 +8,7 @@ public class BlastPoint : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            other.gameObject.GetComponentInParent<Rigidbody>().AddForce(new Vector3(0, 43.5f, 0), ForceMode.Impulse);
+            other.gameObject.GetComponentInParent<Rigidbody>().AddForce(new Vector3(0, 44f, 0), ForceMode.Impulse);
         }
     }
 }
